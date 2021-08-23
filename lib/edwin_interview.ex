ca0e0d57eac3b095eefbce86b30d30d6b3ee1a40
Andy <andy@gettimesaved.com>
2021-08-22 21:31:59 -0400

    Refactor permutation code and add test cases

--- lib/edwin_interview.ex
+++ lib/edwin_interview.ex
@@ -4,30 +4,48 @@ defmodule EdwinInterview do
   """
 
   def calc(users, products, probability_matrix) do
-    users = 1..length(users) |> Enum.to_list()
-
-    permutations = Permutations.subset(users, length(products))
-
-    permutations
-    |> Enum.map(fn permutation ->
-      permutation
-      |> Enum.with_index()
-      |> Enum.map(fn {user_index, product_index} ->
-        probability_matrix
-        |> Enum.at(user_index - 1)
-        |> Enum.at(product_index - 1)
-      end)
-      |> Enum.sum()
-    end)
+    get_permutations(users, products)
+    |> Enum.map(&sum_permutation(&1, probability_matrix))
     |> Enum.max()
+  end
+
+  def calc_with_permutation(users, products, probability_matrix) do
+    {max, best_permutation} =
+      get_permutations(users, products)
+      |> Enum.reduce({0, nil}, fn current_permutation, {prev_max, prev_best} ->
+        current_sum = sum_permutation(current_permutation, probability_matrix)
+
+        if current_sum > prev_max do
+          {current_sum, current_permutation}
+        else
+          {prev_max, prev_best}
+        end
+      end)
+
+    {max, best_permutation}
+  end
+
+  defp get_permutations(users, products) do
+    1..length(users)
+    |> Enum.to_list()
+    |> Permutations.permute(length(products))
+  end
+
+  defp sum_permutation(permutation, probability_matrix) do
+    permutation
+    |> Enum.with_index()
+    |> Enum.map(fn {user_index, product_index} ->
+      probability_matrix
+      |> Enum.at(user_index - 1)
+      |> Enum.at(product_index - 1)
+    end)
+    |> Enum.sum()
+    |> Float.round(2)
   end
 end
 
 defmodule Permutations do
-  def subset([], _), do: [[]]
-  def subset(_, 0), do: [[]]
-
-  def subset(list, i) do
-    for x <- list, y <- subset(list, i - 1), do: [x | y]
-  end
+  def permute([], _), do: [[]]
+  def permute(_, 0), do: [[]]
+  def permute(list, i), do: for(x <- list, y <- permute(list -- [x], i - 1), do: [x | y])
 end
